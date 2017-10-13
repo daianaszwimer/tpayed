d365d4943d9bdbd95eeabf9760f7a400347171d3
DiePecc <32345968+DiePecc@users.noreply.github.com>
2017-10-13 16:59:02 -0300

    Update

--- tp.cpp
+++ tp.cpp
@@ -5,6 +5,7 @@
 #include<time.h>		//el random se basa en la hora actual
 
 #define VEL 40 			//Velocidad promedio del movil
+#define INF 1000
 
 using namespace std;
 
@@ -18,11 +19,24 @@ struct NODO{
 };
 
 void crearMapa(NODO *&P);	//En esta funcion creamos todos los nodos y los interconectamos como pide la consigna
+void actualizarPesos(NODO *&actual, NODO *&adyacente, int i);
 
 int main(){
+	int i=0;
 	srand(time(NULL));		//Basamos el random en la hora actual
 	NODO *puntero=NULL;		//Puntero que va a ir apuntando a cada colegio a medida que avanzemos por el camino
 	crearMapa(puntero);
+	
+	while(puntero->adyacente[i]!=NULL){
+		actualizarPesos(puntero, puntero->adyacente[i], i);
+		i++;
+	}
+	i=0;
+	cout<<puntero->pesosAcumulados<<endl;
+	while(puntero->adyacente[i]!=NULL){
+	cout<<puntero->adyacente[i]->pesosAcumulados<<endl;
+	i++;
+}
 	return 0;
 }
 
@@ -56,6 +70,16 @@ void crearMapa(NODO *&P){
 	C6->etiqueta=false;
 	C7->etiqueta=false;
 	CC->etiqueta=false;
+	
+	CL->pesosAcumulados=0;
+	C1->pesosAcumulados=INF;
+	C2->pesosAcumulados=INF;
+	C3->pesosAcumulados=INF;
+	C4->pesosAcumulados=INF;
+	C5->pesosAcumulados=INF;
+	C6->pesosAcumulados=INF;
+	C7->pesosAcumulados=INF;
+	CC->pesosAcumulados=INF;
 	
 	CL->distancia[0]=4;					//Distancia de CL a C1
 	CL->distancia[1]=2;					//Distancia de CL a C2
@@ -98,34 +122,40 @@ void crearMapa(NODO *&P){
 		CC->demora[i]=((float) rand() / (RAND_MAX))*VEL;
 	}	
 		
-	CL->adyacente[0]=C1;				//Conexión CL-C1
-	CL->adyacente[1]=C2;				//Conexión CL-C2
-	C1->adyacente[0]=CL;				//Conexión C1-CL
-	C1->adyacente[1]=C2;				//Conexión C1-C2
-	C1->adyacente[2]=C3;				//Conexión C1-C3
-	C2->adyacente[0]=CL;				//Conexión C2-CL
-	C2->adyacente[1]=C1;				//Conexión C2-C1
-	C2->adyacente[2]=C3;				//Conexión C2-C3
-	C2->adyacente[3]=C4;				//Conexión C2-C4
-	C3->adyacente[0]=C1;				//Conexión C3-C1
-	C3->adyacente[1]=C2;				//Conexión C3-C2
-	C3->adyacente[2]=C4;				//Conexión C3-C4
-	C3->adyacente[3]=C5;				//Conexión C3-C5
-	C4->adyacente[0]=C2;				//Conexión C4-C2
-	C4->adyacente[1]=C3;				//Conexión C4-C3
-	C4->adyacente[2]=C5;				//Conexión C4-C5
-	C5->adyacente[0]=C3;				//Conexión C5-C3
-	C5->adyacente[1]=C4;				//Conexión C5-C4
-	C5->adyacente[2]=C6;				//Conexión C5-C6
-	C5->adyacente[3]=C7;				//Conexión C5-C7
-	C6->adyacente[0]=C5;				//Conexión C6-C5
-	C6->adyacente[1]=C7;				//Conexión C6-C7
-	C6->adyacente[2]=CC;				//Conexión C6-CC
-	C7->adyacente[0]=C5;				//Conexión C7-C5
-	C7->adyacente[1]=C6;				//Conexión C7-C6
-	C7->adyacente[2]=CC;				//Conexión C7-CC
-	CC->adyacente[1]=C6;				//Conexión CC-C6
-	CC->adyacente[2]=C7;				//Conexión CC-C7	
+	CL->adyacente[0]=C1;				//Conexion CL-C1
+	CL->adyacente[1]=C2;				//Conexion CL-C2
+	C1->adyacente[0]=CL;				//Conexion C1-CL
+	C1->adyacente[1]=C2;				//Conexion C1-C2
+	C1->adyacente[2]=C3;				//Conexion C1-C3
+	C2->adyacente[0]=CL;				//Conexion C2-CL
+	C2->adyacente[1]=C1;				//Conexion C2-C1
+	C2->adyacente[2]=C3;				//Conexion C2-C3
+	C2->adyacente[3]=C4;				//Conexion C2-C4
+	C3->adyacente[0]=C1;				//Conexion C3-C1
+	C3->adyacente[1]=C2;				//Conexion C3-C2
+	C3->adyacente[2]=C4;				//Conexion C3-C4
+	C3->adyacente[3]=C5;				//Conexion C3-C5
+	C4->adyacente[0]=C2;				//Conexion C4-C2
+	C4->adyacente[1]=C3;				//Conexion C4-C3
+	C4->adyacente[2]=C5;				//Conexion C4-C5
+	C5->adyacente[0]=C3;				//Conexion C5-C3
+	C5->adyacente[1]=C4;				//Conexion C5-C4
+	C5->adyacente[2]=C6;				//Conexion C5-C6
+	C5->adyacente[3]=C7;				//Conexion C5-C7
+	C6->adyacente[0]=C5;				//Conexion C6-C5
+	C6->adyacente[1]=C7;				//Conexion C6-C7
+	C6->adyacente[2]=CC;				//Conexion C6-CC
+	C7->adyacente[0]=C5;				//Conexion C7-C5
+	C7->adyacente[1]=C6;				//Conexion C7-C6
+	C7->adyacente[2]=CC;				//Conexion C7-CC
+	CC->adyacente[1]=C6;				//Conexion CC-C6
+	CC->adyacente[2]=C7;				//Conexion CC-C7	
 	
-	P=CL;								//El puntero empieza en el centro de logística
+	P=CL;								//El puntero empieza en el centro de logistica
+}
+
+void actualizarPesos(NODO *&actual, NODO *&adyacente, int i){
+    if(actual->pesosAcumulados + actual->distancia[i] < adyacente->pesosAcumulados){
+        adyacente->pesosAcumulados = actual->pesosAcumulados + actual->distancia[i];
+    }
 }
