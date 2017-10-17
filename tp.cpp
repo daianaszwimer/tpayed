b1e6587206abde875515bd322ba8a05cddfc0625
Daiana Szwimer <daiu.szwimer@gmail.com>
2017-10-16 21:47:09 -0300

    con velocidad ingresada por usuario

--- tp.cpp
+++ tp.cpp
@@ -4,7 +4,6 @@
 #include<string.h>
 #include<time.h>		//el random se basa en la hora actual
 
-#define VEL 40 			//Velocidad promedio del movil
 #define INF 10000		//Definimos "Infinito" para el valor inicial de los pesos
 
 using namespace std;
@@ -17,15 +16,19 @@ struct NODO{
 	float pesoAcumulado;	//Distancia + demora + peso anterior
 };
 
-void crearMapa(NODO *&P);	//En esta funcion creamos todos los nodos y los interconectamos como pide la consigna
+void crearMapa(NODO *&P, float velocidad);	//En esta funcion creamos todos los nodos y los interconectamos como pide la consigna
 void actualizarPesos(NODO *&actual, int i);		//Actualizamos los pesos de los vertices adyacentes de a donde apunta el puntero
 NODO* dijkstra(NODO *&puntero, int j);				//Compara los pesos de los adyacentes, y actualiza el lugar a donde apunta el puntero
 bool chequearAdyacentes(NODO *C, string descripcionActual);		//Devuelve true si tiene por lo menos un colegio adyacente con etiqueta=true sin contar el colegio actual
 
 int main(){
+	float velocidad=0;
+	//validar que no se ingrese un numero < 0
+	cout<<"Ingrese velocidad promedio del móvil"<<endl;
+	cin>>velocidad;
 	srand(time(NULL));		//Basamos el random en la hora actual
 	NODO *puntero=NULL;		//Puntero que va a ir apuntando a cada colegio a medida que avanzemos por el camino
-	crearMapa(puntero);
+	crearMapa(puntero, velocidad);
 	NODO *camino[9];		//Esta variable es la que vamos a mostrar por pantalla con el recorrido que hay que hacer
 	
 	camino[0] = puntero;
@@ -43,7 +46,7 @@ int main(){
 	return 0;
 }
 
-void crearMapa(NODO *&P){
+void crearMapa(NODO *&P, float velocidad){
 	NODO *CL = new NODO();
 	NODO *C1 = new NODO();
 	NODO *C2 = new NODO();
@@ -84,32 +87,32 @@ void crearMapa(NODO *&P){
 	C7->pesoAcumulado=INF;
 	CC->pesoAcumulado=INF;
 	
-	CL->distancia[0]=4+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de CL a C1
-	CL->distancia[1]=2+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de CL a C2
+	CL->distancia[0]=4+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de CL a C1
+	CL->distancia[1]=2+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de CL a C2
 	C1->distancia[0]=CL->distancia[0];	//Distancia de C1 a CL
-	C1->distancia[1]=3+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C1 a C2
-	C1->distancia[2]=4+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C1 a C3
+	C1->distancia[1]=3+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C1 a C2
+	C1->distancia[2]=4+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C1 a C3
 	C2->distancia[0]=CL->distancia[1];	//Distancia de C2 a CL
 	C2->distancia[1]=C1->distancia[1];	//Distancia de C2 a C1
-	C2->distancia[2]=5+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C2 a C3
-	C2->distancia[3]=10+(((float) rand() / (RAND_MAX))*VEL);				//Distancia de C2 a C4
+	C2->distancia[2]=5+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C2 a C3
+	C2->distancia[3]=10+(((float) rand() / (RAND_MAX))*velocidad);				//Distancia de C2 a C4
 	C3->distancia[0]=C1->distancia[2];	//Distancia de C3 a C1
 	C3->distancia[1]=C2->distancia[2];	//Distancia de C3 a C2
-	C3->distancia[2]=1+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C3 a C4
-	C3->distancia[3]=8+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C3 a C5
+	C3->distancia[2]=1+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C3 a C4
+	C3->distancia[3]=8+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C3 a C5
 	C4->distancia[0]=C2->distancia[3];	//Distancia de C4 a C2
 	C4->distancia[1]=C3->distancia[2];	//Distancia de C4 a C3
-	C4->distancia[2]=9+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C4 a C5
+	C4->distancia[2]=9+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C4 a C5
 	C5->distancia[0]=C3->distancia[3];	//Distancia de C5 a C3
 	C5->distancia[1]=C4->distancia[2];	//Distancia de C5 a C4
-	C5->distancia[2]=5+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C5 a C6
-	C5->distancia[3]=6+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C5 a C7
+	C5->distancia[2]=5+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C5 a C6
+	C5->distancia[3]=6+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C5 a C7
 	C6->distancia[0]=C5->distancia[2];	//Distancia de C6 a C5
-	C6->distancia[1]=4+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C6 a C7
-	C6->distancia[2]=2+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C6 a CC
+	C6->distancia[1]=4+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C6 a C7
+	C6->distancia[2]=2+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C6 a CC
 	C7->distancia[0]=C5->distancia[3];	//Distancia de C7 a C5
 	C7->distancia[1]=C6->distancia[1];	//Distancia de C7 a C6
-	C7->distancia[2]=3+(((float) rand() / (RAND_MAX))*VEL);					//Distancia de C7 a CC
+	C7->distancia[2]=3+(((float) rand() / (RAND_MAX))*velocidad);					//Distancia de C7 a CC
 	CC->distancia[0]=C6->distancia[2];	//Distancia de CC a C6
 	CC->distancia[1]=C7->distancia[2];	//Distancia de CC a C7
 		
